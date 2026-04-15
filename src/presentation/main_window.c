952f04e9e4c21cd3ab2b8ede8c304113668955ec
Uriel Cruz Herrera <ucruz4043@gmail.com>
2026-04-15 12:12:05 -0600

    Reacomodo de las pantallas de vista de la interfaz a 900 x 400 px

--- src/presentation/main_window.c
+++ src/presentation/main_window.c
@@ -6,7 +6,7 @@
 GtkWidget *main_window_create(AppState *state) {
     GtkWidget *window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
     gtk_window_set_title(GTK_WINDOW(window), "AviMax");
-    gtk_window_set_default_size(GTK_WINDOW(window), 700, 600);
+    gtk_window_set_default_size(GTK_WINDOW(window), 900, 400);
     gtk_container_set_border_width(GTK_CONTAINER(window), 24);
     gtk_style_context_add_class(gtk_widget_get_style_context(window), "app-window");
 
